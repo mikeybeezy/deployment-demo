077f5e83e6aec5bcbd2eb9952d9e9139bc3c19f5
Michael Babatunde <mikesablaze@Michaels-MacBook-Pro-2.local>
2022-11-15 19:56:28 +0000

    added config for K8's environment

--- terraform/lke-cluster/main.tf
+++ terraform/lke-cluster/main.tf
@@ -1,50 +1,73 @@
+provider "google" {
+  project     = "scaletific-dev-env-368717"
+  credentials = file("vredentials.json")
+  region      = "us-central1"
+  zone        = "us-central1-c"
+}
 terraform {
-  required_providers {
-    linode = {
-      source = "linode/linode"
-      version = "1.16.0"
-    }
+  backend "gcs" {
+    bucket = "scaletific-dev"
+    prfix  = "terrform/state"
   }
 }
-//Use the Linode Provider
-provider "linode" {
-  token = var.token
+
+
+resource "google_project_service" "compute" {
+  service = "compute.googleapis.com"
+
 }
 
-//Use the linode_lke_cluster resource to create
-//a Kubernetes cluster
-resource "linode_lke_cluster" "cool_linode_cluster" {
-    k8s_version = var.k8s_version
-    label = var.label
-    region = var.region
-    tags = var.tags
+resource "google_project_service" "container " {
+  service = "container.googleapis.com"
 
-    dynamic "pool" {
-        for_each = var.pools
-        content {
-            type  = pool.value["type"]
-            count = pool.value["count"]
-        }
-    }
 }
 
-output "kubeconfig" {
-   value = linode_lke_cluster.cool_linode_cluster.kubeconfig
-   sensitive = true
+resource "google_composer_network" "main" {
+  name                    = "main"
+  routing_mode            = "REGIONAL"
+  auto_create_subnetworks = false
+
 }
 
-output "api_endpoints" {
-   value = linode_lke_cluster.cool_linode_cluster.api_endpoints
-}
+# //Use the Linode Provider
+# provider "linode" {
+#   token = var.token
+# }
 
-output "status" {
-   value = linode_lke_cluster.cool_linode_cluster.status
-}
+# //Use the linode_lke_cluster resource to create
+# //a Kubernetes cluster
+# resource "linode_lke_cluster" "cool_linode_cluster" {
+#     k8s_version = var.k8s_version
+#     label = var.label
+#     region = var.region
+#     tags = var.tags
 
-output "id" {
-   value = linode_lke_cluster.cool_linode_cluster.id
-}
+#     dynamic "pool" {
+#         for_each = var.pools
+#         content {
+#             type  = pool.value["type"]
+#             count = pool.value["count"]
+#         }
+#     }
+# }
 
-output "pool" {
-   value = linode_lke_cluster.cool_linode_cluster.pool
-}
+# output "kubeconfig" {
+#    value = linode_lke_cluster.cool_linode_cluster.kubeconfig
+#    sensitive = true
+# }
+
+# output "api_endpoints" {
+#    value = linode_lke_cluster.cool_linode_cluster.api_endpoints
+# }
+
+# output "status" {
+#    value = linode_lke_cluster.cool_linode_cluster.status
+# }
+
+# output "id" {
+#    value = linode_lke_cluster.cool_linode_cluster.id
+# }
+
+# output "pool" {
+#    value = linode_lke_cluster.cool_linode_cluster.pool
+# }
