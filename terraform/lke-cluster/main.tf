74a3b2e4f2569b2fa6eec3e25a639b7862585071
Mike Babs <mikeybabs40@gmail.com>
2022-11-16 02:26:58 +0000

    added latest iteration of terraform config for k8's cluster

--- terraform/lke-cluster/main.tf
+++ terraform/lke-cluster/main.tf
@@ -1,13 +1,13 @@
 provider "google" {
   project     = "scaletific-dev-env-368717"
-  credentials = file("vredentials.json")
+  credentials = file("credentials.json")
   region      = "us-central1"
   zone        = "us-central1-c"
 }
 terraform {
   backend "gcs" {
     bucket = "scaletific-dev"
-    prfix  = "terrform/state"
+    prefix = "terrform/state"
   }
 }
 
@@ -16,7 +16,7 @@ resource "google_project_service" "compute" {
 
 }
 
-resource "google_project_service" "container " {
+resource "google_project_service" "container" {
   service = "container.googleapis.com"
 
 }
@@ -45,17 +45,17 @@ resource "google_compute_subnetwork" "private" {
   private_ip_google_access = true
 }
 
-# kubernets pods wil use IP addres from the secondary ip range this can also be used for incase we need to open a firewall tp access other VM's in the VPC from kubernetes
-secondary_ip_range {
-  range_name    = "k8s-pod-range"
-  ip_cidr_range = "10.48.0.0/14"
-}
+# # kubernets pods wil use IP addres from the secondary ip range this can also be used for incase we need to open a firewall tp access other VM's in the VPC from kubernetes
+# secondary_ip_range {
+#   range_name    = "k8s-pod-range"
+#   ip_cidr_range = "10.48.0.0/14"
+# }
 
-# Used to assign IP addresses for cluster IPS and kubernetes service 
-secondary_ip_range {
-  range_name    = "k8s-service-range"
-  ip_cidr_range = "10.48.0.0/14"
-}
+# # Used to assign IP addresses for cluster IPS and kubernetes service 
+# secondary_ip_range {
+#   range_name    = "k8s-service-range"
+#   ip_cidr_range = "10.48.0.0/14"
+# }
 
 # ROUTER
 # used for VMS without public Ip address to access the internet 
@@ -105,14 +105,127 @@ resource "google_compute_firewall" "allow-ssh" {
 
 resource "google_container_cluster" "primary" {
   name                     = "scaletific-dev"
-  location                 = "us-central1"
+  location                 = "us-central1-a"
   remove_default_node_pool = true
   initial_node_count       = 2
   network                  = google_compute_network.main.self_link
   subnetwork               = google_compute_subnetwork.private.self_link
-  logging_service          = "logging.googleapis.com/Kubernetes"
+  # logging_service          = "logging.googleapis.com/Kubernetes"
+  monitoring_service = "monitoring.googleapis.com/Kubernetes"
+  networking_mode    = "VPC_NATIVE"
+
+  # for multi zonal cluster
+  node_locations = [
+    "us-central1-b"
+  ]
+
+  addons_config {
+    http_load_balancing {
+      disabled = true
+    }
+    horizontal_pod_autoscaling {
+      disabled = false
+    }
+  }
+  release_channel {
+    channel = "REGULAR"
+  }
+
+  workload_identity_config {
+    workload_pool = "scaletific-dev-env-368717"
+  }
+
+  ip_allocation_policy {
+    cluster_secondary_range_name  = "k8s-pod-range"
+    services_secondary_range_name = "k8-service-range"
+  }
+
+  private_cluster_config {
+    enable_private_nodes    = true
+    enable_private_endpoint = false #set to true if using VPN or bastion host
+    master_ipv4_cidr_block  = "172.16.0.0/28"
+  }
+  # # Jenkins usecase
+  # master_authorized_networks_config {
+  #   cidr_blocks {
+  #     cidr_block = "10.0.0.0/18"
+  #     display_name = "private-subnet-w-jenkins"
+  #   }
+  # }
+}
+
+#kubernetes nodes 
+resource "google_service_account" "scaletific_k8s_dev" {
+  account_id = "scaletific-k8s-dev-env"
 
 }
+
+
+resource "google_container_node_pool" "general" {
+  name       = "general"
+  cluster    = google_container_cluster.primary.id
+  node_count = 1
+
+  management {
+    auto_repair  = true
+    auto_upgrade = true
+  }
+
+  node_config {
+    preemptible  = false
+    machine_type = "e2-small"
+    labels = {
+      role = "general"
+    }
+    service_account = google_service_account.scaletific-k8s-dev.email
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+  }
+
+}
+
+resource "google_container_node_pool" "spot" {
+  name    = "spot"
+  cluster = google_container_cluster.primary.id
+
+  management {
+    auto_repair  = true
+    auto_upgrade = true
+  }
+
+  autoscaling {
+    min_node_count = 0
+    max_node_count = 10
+
+  }
+
+  node_config {
+    preemptible  = true
+    machine_type = "e2-small"
+
+    lables = {
+      team = "devops"
+    }
+
+    taint = [{
+      effect = "NO_SCHEDULE"
+      key    = "instance_type"
+      value  = "spot"
+    }]
+
+    service_account = google_service_account.scaletific-k8s-dev.email
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+  }
+
+}
+
+
+
+
+
 # //Use the Linode Provider
 # provider "linode" {
 #   token = var.token
